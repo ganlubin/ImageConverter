b3f82bb1313e899b26c8ddad6685a2e32a711768
ganlubin <sokeegancn@qq.com>
2023-09-19 16:37:40 +0800

    update

--- image_converter.cpp
+++ image_converter.cpp
@@ -6,17 +6,14 @@ int main(int argc, char** argv) {
         return 1;
     }
 
-    // 读取输入图片
     cv::Mat input_image = cv::imread(argv[1]);
     if (input_image.empty()) {
         std::cerr << "Could not open or find the input image." << std::endl;
         return 2;
     }
 
-    // 提取输出格式
     std::string output_format = argv[3];
 
-    // 保存输出图片
     if (cv::imwrite(argv[2], input_image)) {
         std::cout << "Image saved as " << argv[2] << " in " << output_format << " format." << std::endl;
         return 0;
